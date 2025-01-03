46bf7d1fe51d1b21c3035e7fde10cbe946b36e38
ShreyanshPokharel <shreyansh@epravidi.com>
2025-01-03 20:55:28 +0545

    switch case added

--- sherey.c
+++ sherey.c
@@ -2,9 +2,58 @@
 
 int main(){
 
-    int num;
+    int num, choice;
+    char c;
 
-    printf("Enter a number:");
-    scanf("%d", &num);
-    return 0;
+    do
+  {
+
+    printf("CALCULATORRRR\n");
+
+    printf("Choose Operation:\n1)Addition\n\t2)Subtraction\n\t3)Divison\n\t4)Multiplication\n");
+    scanf("%d", &choice);
+
+    printf("enter two numbers: ");
+    scanf("%d %d", &x, &y);
+
+    switch (choice)
+    {
+    case 1:
+
+      sum();
+
+      break;
+
+    case 2:
+
+      sub();
+
+      break;
+
+    case 3:
+
+      div();
+
+      break;
+
+    case 4:
+
+      mul();
+
+      break;
+
+    default:
+
+      "Invalid choice, Input again.";
+    }
+
+    printf("DO YOU WANT TO USE THE CALCULATOR AGAIN?? (y/n): ");
+    scanf(" %c", &c);
+
+  }
+
+  while (c == 'y');
+
+  return 0;
 }
+
